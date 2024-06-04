59e124de937e3a94ca3ce530d55403c41115368b
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2024-06-04 19:10:37 +0300

    add what-a-helmet-can-do

--- src/js/main.js
+++ src/js/main.js
@@ -61,29 +61,93 @@ new Accordion('.frequently-asked-questions__accordion', {
 });
 
 const viewOfTheHelmet = document.querySelector('.view-of-the-helmet__helmet');
-const viewOfTheHelmetBtn = document.querySelectorAll('.view-of-the-helmet__btn');
+const viewOfTheHelmetBtn = document.querySelectorAll(
+  '.view-of-the-helmet__btn'
+);
 
 if (viewOfTheHelmet && viewOfTheHelmetBtn.length) {
-  viewOfTheHelmetBtn.forEach(btn => {
+  viewOfTheHelmetBtn.forEach((btn) => {
     btn.addEventListener('click', () => {
-      viewOfTheHelmetBtn.forEach(item => {
+      viewOfTheHelmetBtn.forEach((item) => {
         item.classList.remove('view-of-the-helmet__btn_active');
       });
 
-      viewOfTheHelmet.querySelector('img').setAttribute('src', btn.getAttribute('data-img'));
+      viewOfTheHelmet
+        .querySelector('img')
+        .setAttribute('src', btn.getAttribute('data-img'));
       btn.classList.add('view-of-the-helmet__btn_active');
-    })
-  })
+    });
+  });
 }
 
 document.addEventListener('click', function (e) {
   const socialsAccordion = document.querySelector('.socials-accordion');
 
-  if (e.target.classList.contains('socials-accordion__btn') && socialsAccordion) {
+  if (
+    e.target.classList.contains('socials-accordion__btn') &&
+    socialsAccordion
+  ) {
     socialsAccordion.classList.toggle('socials-accordion_open');
   }
 
-  if (!e.target.classList.contains('socials-accordion__btn') && socialsAccordion.classList.contains('socials-accordion_open')) {
+  if (
+    !e.target.classList.contains('socials-accordion__btn') &&
+    socialsAccordion.classList.contains('socials-accordion_open')
+  ) {
     socialsAccordion.classList.remove('socials-accordion_open');
+  }
+
+  const whatHelmet = document.querySelector('.what-a-helmet-can-do');
+  const whatHelmetBtn = whatHelmet.querySelectorAll(
+    '.what-a-helmet-can-do__btn'
+  );
+  const whatHelmetPopup = whatHelmet.querySelectorAll(
+    '.what-a-helmet-can-do__popup'
+  );
+
+  if (whatHelmet && e.target.classList.contains('what-a-helmet-can-do__btn')) {
+    const dataAttr = e.target.getAttribute('data-popup-btn');
+
+    if (!e.target.classList.contains('what-a-helmet-can-do__btn_active')) {
+      whatHelmetBtn.forEach((item) => {
+        item.classList.remove('what-a-helmet-can-do__btn_active');
+      });
+    }
+
+    e.target.classList.toggle('what-a-helmet-can-do__btn_active');
+
+    whatHelmetPopup.forEach((item) => {
+      item.classList.remove('popup-helmet_active');
+
+      if (
+        item.getAttribute('data-popup-wrap') == dataAttr &&
+        e.target.classList.contains('what-a-helmet-can-do__btn_active')
+      ) {
+        item.classList.toggle('popup-helmet_active');
+      }
+    });
+  }
+
+  if (
+    !e.target.closest('.popup-helmet') &&
+    !e.target.closest('.what-a-helmet-can-do__btn')
+  ) {
+    whatHelmetBtn.forEach((item) => {
+      item.classList.remove('what-a-helmet-can-do__btn_active');
+    });
+
+    whatHelmetPopup.forEach((item) => {
+      item.classList.remove('popup-helmet_active');
+    });
+  }
+
+  if (whatHelmet && e.target.classList.contains('popup-helmet__close')) {
+    whatHelmetBtn.forEach((item) => {
+      item.classList.remove('what-a-helmet-can-do__btn_active');
+    });
+
+    whatHelmetPopup.forEach((item) => {
+      item.classList.remove('popup-helmet_active');
+    });
   }
 });

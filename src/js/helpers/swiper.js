f9a41436bbc65d7b93e7880674015dc582e6e54a
Roman Nikolaenko <roma.nikolaenko.91@gmail.com>
2024-06-25 21:54:59 +0300

    add slader reviews

--- src/js/helpers/swiper.js
+++ src/js/helpers/swiper.js
@@ -5,7 +5,7 @@ Swiper.use([Pagination, Navigation, Autoplay, EffectFade]);
 
 const hero1 = document.querySelector('.hero1 .swiper-wrapper');
 
-if(hero1) {
+if (hero1) {
   const dotsName = hero1.getAttribute('data-text').split(',');
   const dotsImage = hero1.getAttribute('data-img').split(',');
 
@@ -14,96 +14,134 @@ if(hero1) {
     slidesPerView: 1,
     spaceBetween: 30,
     speed: 1000,
-    effect: "fade",
+    effect: 'fade',
     autoplay: {
       delay: 12000,
       disableOnInteraction: false,
     },
     pagination: {
-      el: ".hero1__pagination",
+      el: '.hero1__pagination',
       clickable: true,
       renderBullet: function (index, className) {
-        return `<span class="${className}"><img src='${dotsImage[index].trim()}' width='42' height='42' alt loading="lazy"> <span>${dotsName[index].trim()}</span></span>`;
+        return `<span class="${className}"><img src='${dotsImage[
+          index
+        ].trim()}' width='42' height='42' alt loading="lazy"> <span>${dotsName[
+          index
+        ].trim()}</span></span>`;
       },
     },
   });
 }
 
-
-const ourDoctorSwiper = new Swiper('.our-doctors__swiper', {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 30,
-  speed: 2000,
-  autoplay: {
-    delay: 12000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".our-doctors-button-next",
-    prevEl: ".our-doctors-button-prev",
-  },
-  pagination: {
-    el: ".our-doctors-pagination",
-    clickable: true, 
-    dynamicBullets: true,
-  },
-  breakpoints: {
-    576: {
-      slidesPerView: 2,
+const reviewsSwiper = document.querySelector('.reviews__swiper');
+if(reviewsSwiper ) {
+  new Swiper('.reviews__swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    speed: 2000,
+    autoplay: {
+      delay: 12000,
+      disableOnInteraction: false,
     },
-
-    991: {
-      slidesPerView: 2.3,
+    navigation: {
+      nextEl: '.reviews__button-next',
+      prevEl: '.reviews__button-prev',
     },
-
-    1200: {
-      slidesPerView: 2,
+    pagination: {
+      el: '.reviews__pagination',
+      clickable: true,
+      dynamicBullets: true,
     },
-
-    1440: {
-      slidesPerView: 2.3,
-    },
-
-    1680: {
-      slidesPerView: 3,
-    },
-  },
-});
-
-function reportWindowSize() {
-  const slider = document.querySelector(".our-doctors__swiper");
-
-  if(slider) {
-    const sliderSlides = slider.querySelectorAll(".swiper-slide");
-
-    if(window.innerWidth >= 320) {
-      if(sliderSlides.length <2) {
-        slider.classList.add('our-doctors__swiper-init')
-      } else {
-        slider.classList.remove('our-doctors__swiper-init')
-      }
-    }
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
   
-    if(window.innerWidth >= 576) {
-      if(sliderSlides.length <3) {
-        slider.classList.add('our-doctors__swiper-init')
-      } else {
-        slider.classList.remove('our-doctors__swiper-init')
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    },
+  });
+}
+
+const ourDoctorSwiper = document.querySelector('.our-doctors__swiper');
+if (ourDoctorSwiper) {
+  new Swiper('.our-doctors__swiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    speed: 2000,
+    autoplay: {
+      delay: 12000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: '.our-doctors-button-next',
+      prevEl: '.our-doctors-button-prev',
+    },
+    pagination: {
+      el: '.our-doctors-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+      },
+
+      991: {
+        slidesPerView: 2.3,
+      },
+
+      1200: {
+        slidesPerView: 2,
+      },
+
+      1440: {
+        slidesPerView: 2.3,
+      },
+
+      1680: {
+        slidesPerView: 3,
+      },
+    },
+  });
+
+  function reportWindowSize() {
+    const slider = document.querySelector('.our-doctors__swiper');
+
+    if (slider) {
+      const sliderSlides = slider.querySelectorAll('.swiper-slide');
+
+      if (window.innerWidth >= 320) {
+        if (sliderSlides.length < 2) {
+          slider.classList.add('our-doctors__swiper-init');
+        } else {
+          slider.classList.remove('our-doctors__swiper-init');
+        }
       }
-    }
-  
-    if(window.innerWidth >= 1680) {
-      if(sliderSlides.length <4) {
-        slider.classList.add('our-doctors__swiper-init')
-      } else {
-        slider.classList.remove('our-doctors__swiper-init')
+
+      if (window.innerWidth >= 576) {
+        if (sliderSlides.length < 3) {
+          slider.classList.add('our-doctors__swiper-init');
+        } else {
+          slider.classList.remove('our-doctors__swiper-init');
+        }
+      }
+
+      if (window.innerWidth >= 1680) {
+        if (sliderSlides.length < 4) {
+          slider.classList.add('our-doctors__swiper-init');
+        } else {
+          slider.classList.remove('our-doctors__swiper-init');
+        }
       }
     }
   }
-  
+
+  reportWindowSize();
+
+  window.onresize = reportWindowSize;
 }
-
-reportWindowSize();
-
-window.onresize = reportWindowSize;
